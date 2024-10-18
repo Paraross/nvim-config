c1ab88c9039f9aab5518772aa36ae140cb77736a
Paraross <Adrago@onet.pl>
2024-10-18 16:26:46 +0200

    Move highlighting to lsp-config Rebind tab move to also include ctrl

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -41,6 +41,8 @@ return {
 					map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 					-- map("K", vim.lsp.buf.hover, "Hover Documentation")
 					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+					map("<leader>h", vim.lsp.buf.document_highlight, "")
+					map("<leader>H", vim.lsp.buf.clear_references, "")
 
 					local client = vim.lsp.get_client_by_id(event.data.client_id)
 
