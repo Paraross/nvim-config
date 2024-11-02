f2007dc16c8f8f680bd00a9edbc9f81a2e276cb8
Paraross <Adrago@onet.pl>
2024-11-02 12:49:35 +0100

    Change one completion mapping

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -26,7 +26,7 @@ return {
 				mapping = cmp.mapping.preset.insert({
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-k>"] = cmp.mapping.complete(),
+					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
 					["<Tab>"] = cmp.mapping.confirm({ select = true }),
