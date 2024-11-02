312d9400519a1bc9e2e0eaec0a177f4d986d0ebb
Paraross <Adrago@onet.pl>
2024-11-02 12:51:21 +0100

    Add here.term

---
+++ lua/plugins/here_term.lua
@@ -0,0 +1,21 @@
+return {
+	"jaimecgomezz/here.term",
+	opts = {},
+	config = function()
+		require("here-term").setup({
+			startup_command = "",
+			mappings = {
+				toggle = "<C-;>",
+				kill = "<C-A-;>",
+			},
+			extra_mappings = {
+				enable = true,
+				escape = "<C-x>",
+				left = "<C-w>h",
+				down = "<C-w>j",
+				up = "<C-w>k",
+				right = "<C-w>l",
+			},
+		})
+	end,
+}
