8b4e2f0e5d993b1739e2ba802f657cc815bf87a0
Paraross <Adrago@onet.pl>
2025-04-05 14:23:20 +0200

    Add Dart and Flutter support

---
+++ lua/plugins/flutter-tools.lua
@@ -0,0 +1,17 @@
+return {
+	"nvim-flutter/flutter-tools.nvim",
+	lazy = false,
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"stevearc/dressing.nvim", -- optional for vim.ui.select
+	},
+	-- config = true,
+	config = function()
+		vim.keymap.set("n", "<leader>br", function()
+			vim.cmd.tabnew()
+			vim.cmd.FlutterRun("-d windows")
+		end, {})
+
+		require("flutter-tools").setup({})
+	end,
+}
