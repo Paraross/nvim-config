608dcb45daf68b28e04e11792ea405e10a8587fa
Paraross <Adrago@onet.pl>
2025-04-05 15:39:39 +0200

    Chuj do dupy

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -27,16 +27,14 @@ return {
 						vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
 					end
 
-					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-					map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-					map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-					map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-					map(
-						"<leader>ws",
-						require("telescope.builtin").lsp_dynamic_workspace_symbols,
-						"[W]orkspace [S]ymbols"
-					)
+					local builtin = require("telescope.builtin")
+					-- map("gd", builtin.lsp_definitions, "[G]oto [D]efinition")
+					map("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+					map("gr", builtin.lsp_references, "[G]oto [R]eferences")
+					map("gI", builtin.lsp_implementations, "[G]oto [I]mplementation")
+					map("<leader>D", builtin.lsp_type_definitions, "Type [D]efinition")
+					map("<leader>ds", builtin.lsp_document_symbols, "[D]ocument [S]ymbols")
+					map("<leader>ws", builtin.lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
 					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 					map("<leader>h", vim.lsp.buf.document_highlight, "")
 					map("<leader>H", vim.lsp.buf.clear_references, "")
