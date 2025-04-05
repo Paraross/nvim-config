63f34ffe14ffbb2518fb5355919dabf6b05d0814
Paraross <Adrago@onet.pl>
2025-04-05 16:18:06 +0200

    Fuck around with tabs and spaces

--- lua/plugins/flutter-tools.lua
+++ lua/plugins/flutter-tools.lua
@@ -7,6 +7,9 @@ return {
 	},
 	-- config = true,
 	config = function()
+		vim.opt.tabstop = 2
+		vim.opt.softtabstop = 2
+
 		vim.keymap.set("n", "<leader>br", function()
 			vim.cmd.tabnew()
 			vim.cmd.FlutterRun("-d windows")
