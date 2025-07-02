09b647a3922a1bd54c9a1e6d9fcf83b4dd3f3a8d
Paraross <Adrago@onet.pl>
2025-07-02 16:03:54 +0200

    Add nvim-notify

---
+++ lua/plugins/notify.lua
@@ -0,0 +1,13 @@
+return {
+	"rcarriga/nvim-notify",
+	config = function()
+		local notify = require("notify")
+		notify.setup({
+			background_colour = "#000000",
+		})
+		-- notify.setup({
+		-- 	background_colour = "#121212",
+		-- })
+		vim.notify = notify
+	end,
+}
