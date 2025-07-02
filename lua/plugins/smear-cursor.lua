564bdc657a06f953cbdc9593d8d05c4f92e79b03
Paraross <Adrago@onet.pl>
2025-07-02 17:17:07 +0200

    Add smear-cursor

---
+++ lua/plugins/smear-cursor.lua
@@ -0,0 +1,10 @@
+return {
+	"sphamba/smear-cursor.nvim",
+	opts = {
+		stiffness = 0.5,
+		trailing_stiffness = 0.5,
+		damping = 1.0,
+		transparent_bg_fallback_color = "#000000",
+		time_interval = 7,
+	},
+}
