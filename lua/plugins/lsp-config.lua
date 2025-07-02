df8aeebc6647eedca37833d1d7bcbd0e0bcf1e0f
Paraross <Adrago@onet.pl>
2025-07-02 15:33:32 +0200

    Remove commented code

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -40,34 +40,6 @@ return {
 
 					local client = vim.lsp.get_client_by_id(event.data.client_id)
 
-					-- The following two autocommands are used to highlight references of the
-					-- word under your cursor when your cursor rests there for a little while.
-					--    See `:help CursorHold` for information about when this is executed
-					-- When you move your cursor, the highlights will be cleared (the second autocommand).
-					-- if client and client.server_capabilities.documentHighlightProvider then
-					-- local highlight_augroup =
-					-- 	vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
-					-- vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-					-- 	buffer = event.buf,
-					-- 	group = highlight_augroup,
-					-- 	callback = vim.lsp.buf.document_highlight,
-					-- })
-					--
-					-- vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-					-- 	buffer = event.buf,
-					-- 	group = highlight_augroup,
-					-- 	callback = vim.lsp.buf.clear_references,
-					-- })
-					--
-					-- vim.api.nvim_create_autocmd("LspDetach", {
-					-- 	group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
-					-- 		callback = function(event2)
-					-- 			vim.lsp.buf.clear_references()
-					-- 			vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
-					-- 		end,
-					-- 	})
-					-- end
-
 					-- The following autocommand is used to enable inlay hints
 					if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
 						map("<leader>th", function()
@@ -76,10 +48,6 @@ return {
 					end
 				end,
 			})
-
-			--local capabilities = vim.lsp.protocol.make_client_capabilities()
-			--capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-			--require("mason").setup()
 
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
