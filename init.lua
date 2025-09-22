0ddb47c32fc369119cf96dad6ecd2baefc2abfc9
Paraross <adrago@onet.pl>
2025-09-22 08:38:45 +0200

    Highlight yanked text

--- init.lua
+++ init.lua
@@ -19,4 +19,11 @@ local opts = {}
 require("options")
 require("keymaps")
 
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		-- vim.highlight.on_yank()
+		vim.highlight.on_yank({ higroup = "Search", timeout = 350 })
+	end,
+})
+
 require("lazy").setup("plugins", opts)
