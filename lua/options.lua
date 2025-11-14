9716e7f70f5fab59b04cf3bc04c13ea91044a1e7
Paraross <adrago@onet.pl>
2025-11-14 14:15:23 +0100

    Change shell to nu from pwsh

--- lua/options.lua
+++ lua/options.lua
@@ -7,7 +7,7 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 0
 -- shell
-vim.opt.shell = "pwsh"
+vim.opt.shell = "nu"
 vim.opt.shellcmdflag = "-c"
 vim.opt.shellquote = ""
 vim.opt.shellxquote = ""
