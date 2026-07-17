667aa28a300061e36a692a25c8afd7e4d8bdff7f
Paraross <adrago@onet.pl>
2026-07-17 10:34:36 +0200

    Add color code colorization

---
+++ lua/plugins/colorizer.lua
@@ -0,0 +1,9 @@
+return {
+	"catgoose/nvim-colorizer.lua",
+	event = "BufReadPre",
+	config = function()
+		require("colorizer").setup()
+
+		vim.keymap.set("n", "<leader>ct", "<CMD>ColorizerToggle<CR>")
+	end,
+}
