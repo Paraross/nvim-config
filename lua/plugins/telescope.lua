3a282715d43660d4c8dd91a90b52f02c02f03603
Paraross <adrago@onet.pl>
2026-09-25 09:43:12 +0200

    Map telescope git pickers

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,8 +4,12 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			vim.keymap.set("n", "<C-p>", builtin.find_files)
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep)
+			vim.keymap.set("n", "<leader>gc", builtin.git_commits)
+			vim.keymap.set("n", "<leader>gb", builtin.git_bcommits)
+			vim.keymap.set({ "n", "v" }, "<leader>gr", builtin.git_bcommits_range)
+			vim.keymap.set("n", "<leader>gs", builtin.git_stash)
 		end,
 	},
 	{
